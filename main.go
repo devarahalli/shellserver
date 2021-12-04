c25ba52923fd38b900be67cad40493b64dece499
Prakash D <prakashd@Prakashs-MacBook-Pro.local>
2021-12-04 15:05:24 +0530

    the begining

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+// Start a web server
+// Define API to receive a command from the user
+// Autenticate the user using the Basic authentication method.
+// a. Supported authentication method is 1. Basic 2. Vault token based.
+// Verify the user group.
+// Check the command in the policiy server before allowing the command execution from the user.
+//Check the user is
+
+func mani() {
+
+}
